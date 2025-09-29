dd34ad0d1d424357a8a5d675e19f19cdfefcfb54
CODSensei <krishna2342005@gmail.com>
2025-09-29 19:03:17 +0530

    OOPS: Encapsulation

---
+++ OOPS/Encapsulation.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class SportsCar {
+private: 
+	// characters
+	string brand;
+	string model;
+	bool isEngineOn;
+	int currentSpeed;
+	int currentGear;
+
+public:
+	// constructer
+	SportsCar(string b, string m) {
+		this->brand = b;
+		this->model = m;
+		isEngineOn = false;
+		currentSpeed = 0;
+		currentGear = 0;
+	}
+
+	// getters and setters
+	int getCurrentSpeed() {
+		return this->currentSpeed;
+	}
+
+	void startEngine() {
+		isEngineOn = true;
+		cout << brand << " " << model << " : Engine starts with roar!" << endl;
+	}
+
+	void shiftGear(int gear) {
+		if (!isEngineOn){
+			cout << brand << " " << model << " : Engine is off! Cannot Shift Gear." << endl;
+			return;
+		}
+		currentGear = gear;
+		cout << brand << " " << model << " : Shifted to gear " << currentGear << endl;
+	}
+
+	void accelerate() {
+		if (!isEngineOn){
+			cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
+			return;
+		}
+		currentSpeed += 20;
+		cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h" <<  endl;
+	}
+
+	void brake() {
+		currentSpeed -= 20;
+		if (currentSpeed < 0) currentSpeed = 0;
+		cout << brand << " " << model << " : Braking! Speed is now " << currentSpeed << " km/h" <<  endl;
+	}
+
+	void stopEngine() {
+		isEngineOn = false;
+		currentSpeed = 0;
+		currentGear = 0;
+		cout << brand << " " << model << " : Engine turned off!" << endl;
+	}
+};
+
+int main() {
+	
+	SportsCar* myCar = new SportsCar("BMW", "M5CS");
+
+	myCar->startEngine();
+	myCar->shiftGear(1);
+	myCar->accelerate();
+	myCar->shiftGear(2);
+	myCar->accelerate();
+	myCar->brake();
+	myCar->shiftGear(1);
+	myCar->brake();
+	myCar->stopEngine();
+	
+	// setting random speed if public
+	// myCar->currentSpeed = 100;
+	// cout << "Current Speed of my BMW M5CS is set to " << myCar->currentSpeed << endl;
+
+	cout << "Current Speed of my BMW M5CS is " << myCar->getCurrentSpeed() << endl;
+
+	delete myCar;
+	
+	return 0;
+}
